9df497ef63aa624825580da373740f01e08b839e
Soroosh-Hemati <sorooshhemati4@gmail.com>
2024-05-27 22:48:27 +0330

    Add Todo Edit feature

--- public/app.js
+++ public/app.js
@@ -4,31 +4,48 @@ const todoForm = document.querySelector(".todo-form");
 const todoInput = document.querySelector(".todo-form .input-box");
 const todoList = document.querySelector(".todo-list");
 const selectTodos = document.querySelector(".filter-todos");
+const modal = document.querySelector(".modal");
+const modalCloseBtn = document.querySelector(".modal-close-btn");
+const modalForm = document.querySelector(".modal-form");
+const modalInput = document.querySelector(".hidden-input-box");
+const editBtn = document.querySelector("#edit-btn");
+
+editBtn.addEventListener("click", editTodoItem);
+
 todoForm.addEventListener("submit", addNewTodo);
+
 selectTodos.addEventListener("change", (e) => {
   filterValue = e.target.value;
   filterTodos();
 });
-document.addEventListener("DOMContentLoaded", (e) => {
+
+document.addEventListener("DOMContentLoaded", () => {
   const todos = getAllTodos();
   showTodos(todos);
 });
+
+modalCloseBtn.addEventListener("click", () =>
+  modal.classList.remove("show-modal")
+);
 
 function showTodos(todos) {
   let result = "";
   todos.forEach((todo) => {
     result += `<li class="todo bg-gray-100 rounded-lg flex flex-wrap p-3 mb-2">
-    <p class="todo-title ${todo.isCompleted && "completed"} w-2/5">${
+    <p class="todo-title ${todo.isCompleted && "completed"} w-2/6">${
       todo.title
     }</p>
-    <span class="todo-createdAt w-1/5">${new Date(
+    <span class="todo-createdAt w-1/6">${new Date(
       todo.createdAt
     ).toLocaleDateString("fa-IR")}</span>
-    <button data-todo-id=${todo.id} class="todo-check w-1/5">
+    <button data-todo-id=${todo.id} class="todo-check w-1/6">
       <i class="fa fa-check"></i>
     </button>
-    <button data-todo-id=${todo.id} class="todo-remove w-1/5">
+    <button data-todo-id=${todo.id} class="todo-remove w-1/6">
       <i class="fa fa-trash"></i>
+    </button>
+    <button data-todo-id=${todo.id} class="todo-edit w-1/6">
+      <i class="fa fa-pencil"></i>
     </button>
   </li>`;
   });
@@ -39,6 +56,8 @@ function showTodos(todos) {
   removeBtns.forEach((btn) => btn.addEventListener("click", removeTodo));
   const checkBtns = [...document.querySelectorAll(".todo-check")];
   checkBtns.forEach((btn) => btn.addEventListener("click", checkTodos));
+  const editBtns = [...document.querySelectorAll(".todo-edit")];
+  editBtns.forEach((btn) => btn.addEventListener("click", editTodo));
 }
 
 function addNewTodo(e) {
@@ -54,6 +73,7 @@ function addNewTodo(e) {
   saveTodo(newTodo);
   filterTodos();
 }
+
 function filterTodos() {
   const todos = getAllTodos();
   switch (filterValue) {
@@ -76,6 +96,7 @@ function filterTodos() {
     }
   }
 }
+
 function removeTodo(e) {
   let todos = getAllTodos();
   const dataId = Number(e.target.dataset.todoId);
@@ -83,6 +104,7 @@ function removeTodo(e) {
   saveAllTodos(todos);
   filterTodos();
 }
+
 function checkTodos(e) {
   const todos = getAllTodos();
   const dataId = Number(e.target.dataset.todoId);
@@ -91,16 +113,57 @@ function checkTodos(e) {
   saveAllTodos(todos);
   filterTodos();
 }
+
+const showModal = () => {
+  modal.classList.add("show-modal");
+};
+
+function editTodo(e) {
+  let todos = getAllTodos();
+  const dataId = Number(e.target.dataset.todoId);
+  const editedTodo = todos.find((todo) => todo.id == dataId);
+  modalInput.value = editedTodo.title;
+  localStorage.setItem("toEditTodo", JSON.stringify(editedTodo));
+  showModal();
+}
+
 function getAllTodos() {
   const savedTodos = JSON.parse(localStorage.getItem("todos")) || [];
   return savedTodos;
 }
+
 function saveTodo(todo) {
   const savedTodos = getAllTodos();
   savedTodos.push(todo);
   localStorage.setItem("todos", JSON.stringify(savedTodos));
   return savedTodos;
 }
-function saveAllTodos(todos){
+
+function saveAllTodos(todos) {
   localStorage.setItem("todos", JSON.stringify(todos));
+}
+
+function editTodoItem() {
+  const savedTodo = localStorage.getItem("toEditTodo");
+  const parsedTodo = JSON.parse(savedTodo) || undefined;
+  let todos = getAllTodos();
+  const inputModalValue = modalInput.value;
+
+  if (inputModalValue.length === 0) {
+    alert("cant be empty!");
+    return;
+  }
+
+  if (parsedTodo) {
+    todos = todos.map((item) => {
+      if (item.id === parsedTodo.id) {
+        return { ...item, title: inputModalValue };
+      }
+      return item;
+    });
+  }
+
+  saveAllTodos(todos);
+  filterTodos();
+  modal.classList.remove("show-modal");
 }
